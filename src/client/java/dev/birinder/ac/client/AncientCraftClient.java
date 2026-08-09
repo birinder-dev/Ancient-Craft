35ec32d5731d06b5a4078358af709b6e1e9c9d84
Birinder <chhabrabirinder90@gmail.com>
2026-08-09 19:38:49 +0530

    Initial Fabric mod setup

---
+++ src/client/java/dev/birinder/ac/client/AncientCraftClient.java
@@ -0,0 +1,10 @@
+package dev.birinder.ac.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AncientCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
